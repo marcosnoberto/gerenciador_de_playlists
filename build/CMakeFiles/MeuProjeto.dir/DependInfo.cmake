4dc78581f13761ceeee8e09b19ff12063c7310b4
Marcos Vinicius Noberto de Oliveira <112330160+marcosnoberto@users.noreply.github.com>
2023-06-28 22:22:26 -0300

    Add files via upload

---
+++ build/CMakeFiles/MeuProjeto.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/mnt/c/users/vinicios/desktop/gerenciador1/src/Musica.cpp" "CMakeFiles/MeuProjeto.dir/src/Musica.cpp.o" "gcc" "CMakeFiles/MeuProjeto.dir/src/Musica.cpp.o.d"
+  "/mnt/c/users/vinicios/desktop/gerenciador1/src/Playlist.cpp" "CMakeFiles/MeuProjeto.dir/src/Playlist.cpp.o" "gcc" "CMakeFiles/MeuProjeto.dir/src/Playlist.cpp.o.d"
+  "/mnt/c/users/vinicios/desktop/gerenciador1/src/main.cpp" "CMakeFiles/MeuProjeto.dir/src/main.cpp.o" "gcc" "CMakeFiles/MeuProjeto.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/MeuProjeto.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MeuProjeto.dir/src/Musica.cpp.o"
+  "CMakeFiles/MeuProjeto.dir/src/Musica.cpp.o.d"
+  "CMakeFiles/MeuProjeto.dir/src/Playlist.cpp.o"
+  "CMakeFiles/MeuProjeto.dir/src/Playlist.cpp.o.d"
+  "CMakeFiles/MeuProjeto.dir/src/main.cpp.o"
+  "CMakeFiles/MeuProjeto.dir/src/main.cpp.o.d"
+  "MeuProjeto"
+  "MeuProjeto.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MeuProjeto.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
